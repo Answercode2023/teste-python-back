d90173533f9e911f39054da610e5b3f316c8ed7f
Answercode2023 <answer.code.suporte@gmail.com>
2025-08-05 00:13:29 -0300

    adicione o domínio do Render

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9&@-ovccp#4f5q8*l#*&*wt%i1zrxw*#&^e2y%is)t7m*vye@f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['answer-site.onrender.com']
 
 
 # Application definition
