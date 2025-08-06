3f68d05ddafb639409e284c90280edde23a0afb8
Answercode2023 <answer.code.suporte@gmail.com>
2025-08-06 10:02:51 -0300

    Impede deletar categoria se estiver sendo usada

--- tasks/models.py
+++ tasks/models.py
@@ -11,11 +11,11 @@ class Category(models.Model):  # opcional
         return self.name
 
 class Task(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     is_completed = models.BooleanField(default=False)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
